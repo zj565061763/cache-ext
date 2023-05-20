5931cb5832ede120e06399d02f3d8dd4945421c7
zj565061763 <565061763@qq.com>
2023-05-20 19:46:06 +0800

    1.0.0-alpha05

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "cache-ext"
-val libVersionName = "1.0.0-alpha04"
+val libVersionName = "1.0.0-alpha05"
 
 android {
     namespace = "com.sd.lib.cache.ext"
