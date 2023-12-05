e76df9789bdba690b83c2e5fab5778b5315f8738
zjun <565061763@qq.com>
2023-12-05 23:25:44 +0800

    flow

--- lib/src/main/java/com/sd/lib/cache/ext/single/FlowSingleCache.kt
+++ lib/src/main/java/com/sd/lib/cache/ext/single/FlowSingleCache.kt
@@ -4,7 +4,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
 
 abstract class FlowSingleCache<T>(clazz: Class<T>) : SingleCache<T>(clazz) {
     private val _flow = MutableSharedFlow<T>(
@@ -12,39 +11,25 @@ abstract class FlowSingleCache<T>(clazz: Class<T>) : SingleCache<T>(clazz) {
         onBufferOverflow = BufferOverflow.DROP_OLDEST,
     )
 
-    private val _readonlyFlow by lazy {
-        synchronized(clazz) {
-            if (_flow.replayCache.isEmpty()) {
-                syncFlowIfEmpty()
-            }
-        }
-        _flow.asSharedFlow()
-    }
-
     fun flow(): Flow<T> {
-        return _readonlyFlow
+        return _flow
     }
 
     final override fun get(): T? {
         synchronized(clazz) {
-            return _readonlyFlow.replayCache.lastOrNull() ?: syncFlowIfEmpty()
+            return _flow.replayCache.lastOrNull() ?: super.get()?.also {
+                _flow.tryEmit(it)
+            }
         }
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     final override fun onCacheChanged(cache: T?) {
-        super.onCacheChanged(cache)
-        if (cache != null) {
-            _flow.tryEmit(cache)
-        } else {
-            _flow.resetReplayCache()
-        }
-    }
-
-    private fun syncFlowIfEmpty(): T? {
-        return super.get()?.also {
-            if (_flow.replayCache.isEmpty()) {
-                _flow.tryEmit(it)
+        synchronized(clazz) {
+            if (cache != null) {
+                _flow.tryEmit(cache)
+            } else {
+                _flow.resetReplayCache()
             }
         }
     }
