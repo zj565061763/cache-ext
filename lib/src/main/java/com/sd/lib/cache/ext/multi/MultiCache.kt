fcc5a313616a88be7f4c30a36ee6e93d2d1ca0dc
zjun <565061763@qq.com>
2024-01-01 12:30:44 +0800

    comment

--- lib/src/main/java/com/sd/lib/cache/ext/multi/MultiCache.kt
+++ lib/src/main/java/com/sd/lib/cache/ext/multi/MultiCache.kt
@@ -1,5 +1,10 @@
 package com.sd.lib.cache.ext.multi
 
+import kotlinx.coroutines.Dispatchers
+
+/**
+ * 多缓存管理接口，所有方法均在[Dispatchers.IO]上执行
+ */
 interface IMultiCache<T> {
     /**
      * 保存
@@ -17,7 +22,7 @@ interface IMultiCache<T> {
     suspend fun remove(key: String)
 
     /**
-     * 编辑
+     * 编辑，[block]为原子性操作
      */
     suspend fun <R> edit(block: suspend IMultiCache<T>.() -> R): R
 }
