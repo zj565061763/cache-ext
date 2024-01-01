79edc1bcbf0844111c37c3de0081f20eb449b3d1
zjun <565061763@qq.com>
2024-01-01 13:57:39 +0800

    1.0.0-alpha13

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "cache-ext"
-val libVersionName = "1.0.0-alpha12"
+val libVersionName = "1.0.0-alpha13"
 
 android {
     namespace = "com.sd.lib.cache.ext"
