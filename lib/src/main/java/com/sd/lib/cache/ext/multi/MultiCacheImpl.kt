081601de1a0232d6fa7e8c0e39930f1a9e52c1f3
zjun <565061763@qq.com>
2024-01-06 17:44:27 +0800

    create default

--- lib/src/main/java/com/sd/lib/cache/ext/multi/MultiCacheImpl.kt
+++ lib/src/main/java/com/sd/lib/cache/ext/multi/MultiCacheImpl.kt
@@ -3,41 +3,47 @@ package com.sd.lib.cache.ext.multi
 import com.sd.lib.cache.Cache
 import com.sd.lib.cache.FCache
 import com.sd.lib.cache.ext.cacheEdit
+import kotlinx.coroutines.Dispatchers
 
-class MultiCache<T>(
+open class MultiCache<T>(
     clazz: Class<T>,
     cache: Cache = FCache.get(),
 ) : IMultiCache<T> {
 
     private val _cache = cache.cObjects(clazz)
 
-    override suspend fun put(key: String, value: T?): Boolean {
+    final override suspend fun put(key: String, value: T?): Boolean {
         return edit {
             _cache.put(key, value)
         }
     }
 
-    override suspend fun get(key: String): T? {
+    final override suspend fun get(key: String): T? {
         return edit {
-            _cache.get(key)
+            _cache.get(key) ?: create(key)?.also { put(key, it) }
         }
     }
 
-    override suspend fun remove(key: String) {
+    final override suspend fun remove(key: String) {
         edit {
             _cache.remove(key)
         }
     }
 
-    override suspend fun keys(): Array<String> {
+    final override suspend fun keys(): Array<String> {
         return edit {
             _cache.keys()
         }
     }
 
-    override suspend fun <R> edit(block: suspend IMultiCache<T>.() -> R): R {
+    final override suspend fun <R> edit(block: suspend IMultiCache<T>.() -> R): R {
         return cacheEdit {
             block()
         }
     }
+
+    /**
+     * 如果[get]方法未找到缓存，会尝试调用此方法创建缓存返回，[Dispatchers.IO]上执行
+     */
+    protected open fun create(key: String): T? = null
 }
