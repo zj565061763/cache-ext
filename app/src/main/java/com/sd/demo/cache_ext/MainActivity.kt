ea2754bd3d9faa2363f92e9f37451b86e1567f22
zjun <565061763@qq.com>
2024-01-06 20:14:37 +0800

    demo

--- app/src/main/java/com/sd/demo/cache_ext/MainActivity.kt
+++ app/src/main/java/com/sd/demo/cache_ext/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(_binding.root)
-        getCaches()
+        getFlowCaches()
     }
 
     private fun getCache() {
