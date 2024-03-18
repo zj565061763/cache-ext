2b659cd5c1157b89d5c3daf65e4adfaa53fc602f
zjun <565061763@qq.com>
2024-03-18 15:13:34 +0800

    test

--- app/src/androidTest/java/com/sd/demo/cache_ext/MultiCacheTest.kt
+++ app/src/androidTest/java/com/sd/demo/cache_ext/MultiCacheTest.kt
@@ -2,7 +2,7 @@ package com.sd.demo.cache_ext
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import app.cash.turbine.test
-import com.sd.demo.cache_ext.cache.CachesUser
+import com.sd.demo.cache_ext.cache.CacheUsers
 import com.sd.demo.cache_ext.cache.UserModel
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
@@ -11,23 +11,46 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MultiCacheTest {
+    @Test
+    fun test() = runBlocking {
+        UserModel("default", "default").let { user ->
+            CacheUsers.remove("default")
+            assertEquals(user, CacheUsers.get("default"))
+        }
+
+        UserModel("1", "1").let { user ->
+            assertEquals(true, CacheUsers.put("1", user))
+            assertEquals(user, CacheUsers.get("1"))
+
+            CacheUsers.remove("1")
+            assertEquals(null, CacheUsers.get("1"))
+        }
+
+        UserModel("2", "2").let { user ->
+            assertEquals(true, CacheUsers.put("2", user))
+            assertEquals(user, CacheUsers.get("2"))
+
+            CacheUsers.remove("2")
+            assertEquals(null, CacheUsers.get("2"))
+        }
+    }
 
     @Test
     fun multiCacheFlowTest() = runBlocking {
         val defaultUser = UserModel("default", "default")
-        assertEquals(defaultUser, CachesUser.get("default"))
+        assertEquals(defaultUser, CacheUsers.get("default"))
 
         UserModel("1", "1").let { user ->
-            assertEquals(true, CachesUser.put("1", user))
-            assertEquals(user, CachesUser.get("1"))
-            CachesUser.flow("1").test {
+            assertEquals(true, CacheUsers.put("1", user))
+            assertEquals(user, CacheUsers.get("1"))
+            CacheUsers.flow("1").test {
                 assertEquals(user, awaitItem())
             }
         }
 
-        CachesUser.remove("1")
-        assertEquals(null, CachesUser.get("1"))
-        CachesUser.flow("1").test {
+        CacheUsers.remove("1")
+        assertEquals(null, CacheUsers.get("1"))
+        CacheUsers.flow("1").test {
             assertEquals(null, awaitItem())
         }
     }
