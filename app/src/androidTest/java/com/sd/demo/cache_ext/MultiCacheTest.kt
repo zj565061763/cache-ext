f55df29f977f9027a11e26704d9f9e0bdf91cf5d
zjun <565061763@qq.com>
2024-03-18 15:09:43 +0800

    test

--- app/src/androidTest/java/com/sd/demo/cache_ext/MultiCacheTest.kt
+++ app/src/androidTest/java/com/sd/demo/cache_ext/MultiCacheTest.kt
@@ -2,41 +2,15 @@ package com.sd.demo.cache_ext
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import app.cash.turbine.test
-import com.sd.demo.cache_ext.cache.CacheUser
 import com.sd.demo.cache_ext.cache.CachesUser
 import com.sd.demo.cache_ext.cache.UserModel
 import kotlinx.coroutines.runBlocking
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
-
-    @Test
-    fun singleCacheFlowTest() = runBlocking {
-        val defaultUser = UserModel("default", "default")
-        assertEquals(defaultUser, CacheUser.get())
-
-        UserModel("1", "1").let { user ->
-            assertEquals(true, CacheUser.put(user))
-            assertEquals(user, CacheUser.get())
-            CacheUser.flow().test {
-                assertEquals(user, awaitItem())
-            }
-        }
-
-        CacheUser.remove()
-        assertEquals(defaultUser, CacheUser.get())
-        CacheUser.flow().test {
-            assertEquals(defaultUser, awaitItem())
-        }
-    }
+class MultiCacheTest {
 
     @Test
     fun multiCacheFlowTest() = runBlocking {
